3f421b01ffccd6778a01f1605ff90912952ab417
tykm <tylerkim01@gmail.com>
2023-05-03 19:49:42 -0400

    Built out food dictionary

---
+++ split_bill.py
@@ -0,0 +1,36 @@
+food = {
+    "Tyler": {
+        "Spicy Rigatoni": 24,
+    },
+    "Anuraag": {
+        "Chocolate Milkshake": 8.50,
+        "Veggie Burger": 19.75,
+    },
+    "Kevin": {
+        "Guiness": 8,
+        "Buffalo Chicken Sandwich": 19,
+    },
+    "Jay": {
+        "Doctors Orders": 15,
+        "Oreo Milkshake": 9.50,
+        "Penne Alla Vodka": 24,
+    },
+    "Patricia": {
+        "Gin": 18,
+        "Shrimp Pasta": 28,
+    },
+    "Katie": {
+        "Secret Garden": 15,
+        "Chocolate Milkshake": 8.50,
+        "Chicken Parm": 24,
+    },
+}
+
+shared = {
+    "Truffle Fries": 12,
+    "Non Cash Adjustment": 9.33
+}
+
+subtotal = 242.58
+taxtotal = 258.15
+tiptotal = 309.77
