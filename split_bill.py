261547d2847a8e67a7546fe71b4a77ff53257a44
tykm <tylerkim01@gmail.com>
2023-05-03 20:11:50 -0400

    Total calculated cost is  off of actual total cost

--- split_bill.py
+++ split_bill.py
@@ -31,6 +31,20 @@ shared = {
     "Non Cash Adjustment": 9.33
 }
 
+shared_per_person = sum(shared.values()) / len(food)
+tax_multiplier = 1.06625
+tip_multipler = 1.2
 subtotal = 242.58
 taxtotal = 258.15
 tiptotal = 309.77
+
+running_total = 0
+for person in food:
+    cost = sum(food[person].values()) + shared_per_person
+    cost *= tax_multiplier
+    cost *= tip_multipler
+    cost = round(cost, 2)
+    print(person + ": " + str(cost))
+    running_total += cost
+
+print(running_total)
